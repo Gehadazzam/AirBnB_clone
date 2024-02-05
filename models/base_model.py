7de6ceb04d83d62fa281bbe4e7975db71d3e0b6a
gehad <gehadgamalazzam@gmail.com>
2024-02-05 20:37:54 +0200

    args

--- models/base_model.py
+++ models/base_model.py
@@ -1,30 +1,46 @@
 #!/usr/bin/python3
 """Write a class BaseModel that defines all common attributes/methods for other classes"""
 
+
+
 import uuid
-from datetime import datetime
+from datetime import datetime as dt
 
 class BaseModel:
     """all function in basemodel class"""
-    def __init__(self):
+
+    def __init__(self, *args, **kwargs):
         """id / created_at / updated_at"""
+
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        self.created_at = dt.now()
+        self.updated_at = dt.now()
+        if kwargs != '0':
+            for g, a in kwargs.items():
+                if g == 'created_at' or g == 'updated_at':
+                    self.__dict__[g] = dt.strptime(a, "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[g] = a
+        else:    
+            self.id = str(uuid.uuid4())
+            self.created_at = dt.now()
+            self.updated_at = dt.now()
 
     def __str__(self):
         """should print: {[<class name>] (<self.id>) <self.__dict__>}"""
+
         return ("[{}] ({}) {}").format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """updates the public instance attribute updated_at with the current datetime"""
-        self.updated_at =  datetime.now()
+        """updates the public instance attribute updated_at with the current dt"""
+
+        self.updated_at =  dt.now()
     
     def to_dict(self):
         """returns a dictionary containing all keys/values of __dict__ of the instance"""
+
         my_ob_dict = self.__dict__.copy()
         my_ob_dict["__class__"] = self.__class__.__name__
         my_ob_dict["created_at"] = self.created_at.isoformat()
         my_ob_dict["updated_at"] = self.updated_at.isoformat()
         return my_ob_dict
-
