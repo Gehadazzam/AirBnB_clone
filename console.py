28ee355d693369ea6bc568081430195b29f973cf
gehad <gehadgamalazzam@gmail.com>
2024-02-06 17:09:30 +0200

    console

--- console.py
+++ console.py
@@ -1,1 +1,31 @@
 #!/usr/bin/python3
+"""make a command interpreter using cmd module"""
+
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """class to handle the program"""
+
+    prompt = "(hbnb) "
+
+    def do_quit(self, ar):
+        """quit the command interpreter"""
+
+        return True
+
+    def do_EOF(self, ar):
+        """print new line and exit"""
+
+        print()
+        return True
+
+    def emptyline(self):
+        """nothing just pass"""
+
+        pass
+
+
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
