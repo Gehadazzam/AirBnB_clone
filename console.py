bd612feb562454606c67590ca8112f5ea122ded4
gehad <gehadgamalazzam@gmail.com>
2024-02-06 19:05:38 +0200

    cnosle

--- console.py
+++ console.py
@@ -10,6 +10,8 @@ class HBNBCommand(cmd.Cmd):
     """class to handle the program"""
 
     prompt = "(hbnb) "
+    # List of classes
+    cls = ["BaseModel", "User", "Place", "State", "City", "Amenity", "Review"]
 
     def do_quit(self, ar):
         """quit the command interpreter"""
@@ -76,6 +78,48 @@ class HBNBCommand(cmd.Cmd):
                 else:
                     setattr(storage.all()[k], w[2], w[3])
                     storage.all()[k].save()
+    def do_destroy(self, ar):
+        """if the class exist destroy it"""
+
+        par = ar.split()
+        if not ar:
+            """just print a massage and return"""
+
+            print("** class name missing **")
+            return
+        elif not par[0] in HBNBCommand.cls:
+            """check if the argument one of our cls"""
+
+            print("** class doesn't exist **")
+        elif not par[1]:
+            """check if the id is written"""
+
+            print("** instance id missing **")
+
+        else:
+            k = par[0] + "." + par[1]
+            if k in storage.all():
+                """if it existes in the storage"""
+                storage.all().pop(k)
+                storage.save()
+            else:
+                print("** no instance found **")
+
+    def do_all(self, ar):
+        """Prints all string representation of all instances"""
+
+        par = ar.split()
+        if len(par) < 2:
+            """if it is just add print everything"""
+            print([str(key) for key in storage.all().items()])
+
+        else:
+            if not par[1] in HBNBCommand.cls:
+                """if the word after all not a class name"""
+                print("** class doesn't exist **")
+                return
+            else:
+                print([str(key) for key in storage.all().items()])
 
 
 if __name__ == "__main__":
