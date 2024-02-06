b0b7a1aae623618cc8c606f1ccdd65b72704df77
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-02-06 18:57:04 +0200

    airbnb

--- console.py
+++ console.py
@@ -3,7 +3,8 @@
 
 
 import cmd
-
+from models.base_model import BaseModel
+from models import storage
 
 class HBNBCommand(cmd.Cmd):
     """class to handle the program"""
@@ -25,6 +26,56 @@ class HBNBCommand(cmd.Cmd):
         """nothing just pass"""
 
         pass
+
+    def do_create(self, arg):
+        """Creates a new instance of BaseModel"""
+        if not arg:
+            print("** class name missing **")
+        elif arg not in {'BaseModel'}:
+            print ("** class doesn't exist **")
+        else:
+            i = BaseModel()
+            i.save()
+            print(i.id)
+
+    def do_show(self, arg):
+        """Prints the string representation the class name and id"""
+        if not arg:
+            print("** class name missing **")
+        else:
+            w = arg.split()
+            if w[0] not in {'BaseModel'}:
+                print ("** class doesn't exist **")
+            elif len(w) < 2:
+                print("** instance id missing **")
+            else:
+                k = "{}.{}".format(w[0], w[1])
+                if k not in storage.all():
+                    print("** no instance found **")
+                else:
+                    print(storage.all()[k])
+
+    def to_update(self, arg):
+        """Updates an instance adding or updating attribute"""
+        if not arg:
+            print("** class name missing **")
+        else:
+            w = arg.split()
+            if w[0] not in {'BaseModel'}:
+                print ("** class doesn't exist **")
+            elif len(w) < 2:
+                print("** instance id missing **")
+            else:
+                k = "{}.{}".format(w[0], w[1])
+                if k not in storage.all():
+                    print("** no instance found **")
+                elif len(w) < 3:
+                    print("** attribute name missing **")
+                elif len(w) < 4:
+                    print("** value missing **")
+                else:
+                    setattr(storage.all()[k], w[2], w[3])
+                    storage.all()[k].save()
 
 
 if __name__ == "__main__":
