5ee8233efa16b4ef14427564dd7d30321a5927b4
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-02-10 05:03:09 +0200

    air-a

--- models/base_model.py
+++ models/base_model.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-"""Write a class BaseModel that defines all common attributes/methods for other classes"""
+"""
+Write a class BaseModel that defines
+all common attributes/methods for other classes
+"""
 
 
 import uuid
 from datetime import datetime as dt
 from models import storage
+
 
 class BaseModel:
     """all function in basemodel class"""
@@ -14,10 +18,11 @@ class BaseModel:
         if kwargs is not None and kwargs != {}:
             for k in kwargs:
                 if k == "created_at" or k == "updated_at":
-                    self.__dict__[k] = dt.strptime(kwargs[k], "%Y-%m-%dT%H:%M:%S.%f")
+                    self.__dict__[k] = dt.strptime(
+                      kwargs[k], "%Y-%m-%dT%H:%M:%S.%f")
                 else:
                     self.__dict__[k] = kwargs[k]
-        else:    
+        else:
             self.id = str(uuid.uuid4())
             self.created_at = dt.now()
             self.updated_at = dt.now()
@@ -25,20 +30,24 @@ class BaseModel:
 
     def __str__(self):
         """should print: {[<class name>] (<self.id>) <self.__dict__>}"""
-
-        return ("[{}] ({}) {}").format(self.__class__.__name__, self.id, self.__dict__)
+        return ("[{}] ({}) {}").format(
+          self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """updates the public instance attribute updated_at with the current dt"""
-
-        self.updated_at =  dt.now()
+        """
+        updates the public instance attribute
+        updated_at with the current dt
+        """
+        self.updated_at = dt.now()
         storage.save()
-    
+
     def to_dict(self):
-        """returns a dictionary containing all keys/values of __dict__ of the instance"""
+        """
+        returns a dictionary containing all
+        keys/values of __dict__ of the instance
+        """
         my_ob_dict = self.__dict__.copy()
         my_ob_dict["__class__"] = self.__class__.__name__
         my_ob_dict["created_at"] = my_ob_dict["updated_at"].isoformat()
         my_ob_dict["updated_at"] = my_ob_dict["updated_at"].isoformat()
         return my_ob_dict
-
