c9b062ce5f35b325d5d26aa69a5ff3609a4bdf2a
gehad <gehadgamalazzam@gmail.com>
2024-02-10 18:45:53 +0200

    more tests

--- tests/test_console.py
+++ tests/test_console.py
@@ -98,6 +98,10 @@ class Test_HBNBCommand(unittest.TestCase):
             output = "** class doesn't exist **"
             self.assertEqual(output, test.getvalue().strip())
 
+        with patch("sys.stdout", new=SO()) as test:
+            self.assertFalse(HB().onecmd("USER.create()"))
+            output = "** class doesn't exist **"
+            self.assertEqual(output, test.getvalue().strip())
 
     def test_show(self):
         with patch("sys.stdout", new=SO()) as test:
@@ -111,10 +115,71 @@ class Test_HBNBCommand(unittest.TestCase):
             self.assertEqual(output, test.getvalue().strip())
 
         with patch("sys.stdout", new=SO()) as test:
+            self.assertFalse(HB().onecmd("USER.show()"))
+            output = "** class doesn't exist **"
+            self.assertEqual(output, test.getvalue().strip())
+
+        with patch("sys.stdout", new=SO()) as test:
             self.assertFalse(HB().onecmd("show User"))
             output = "** instance id missing **"
             self.assertEqual(output, test.getvalue().strip())
 
+        with patch("sys.stdout", new=SO()) as test:
+            self.assertFalse(HB().onecmd("create User"))
+            id_attribute = test.getvalue().strip()
+        with patch("sys.stdout", new=SO()) as test:
+            cls = storage.all()["User.{}".format(id_attribute)]
+            cmd = "User.show({})".format(id_attribute)
+            self.assertFalse(HB().onecmd(cmd))
+            self.assertNotIn(cls, storage.all())
+        with patch("sys.stdout", new=SO()) as test:
+            self.assertFalse(HB().onecmd("create BaseModel"))
+            id_attribute = test.getvalue().strip()
+        with patch("sys.stdout", new=SO()) as test:
+            cls = storage.all()["BaseModel.{}".format(id_attribute)]
+            cmd = "BaseModel.show({})".format(id_attribute)
+            self.assertFalse(HB().onecmd(cmd))
+            self.assertNotIn(cls, storage.all())
+        with patch("sys.stdout", new=SO()) as test:
+            self.assertFalse(HB().onecmd("create Place"))
+            id_attribute = test.getvalue().strip()
+        with patch("sys.stdout", new=SO()) as test:
+            cls = storage.all()["Place.{}".format(id_attribute)]
+            cmd = "Place.show({})".format(id_attribute)
+            self.assertFalse(HB().onecmd(cmd))
+            self.assertNotIn(cls, storage.all())
+        with patch("sys.stdout", new=SO()) as test:
+            self.assertFalse(HB().onecmd("create Amenity"))
+            id_attribute = test.getvalue().strip()
+        with patch("sys.stdout", new=SO()) as test:
+            cls = storage.all()["Amenity.{}".format(id_attribute)]
+            cmd = "Amenity.show({})".format(id_attribute)
+            self.assertFalse(HB().onecmd(cmd))
+            self.assertNotIn(cls, storage.all())
+        with patch("sys.stdout", new=SO()) as test:
+            self.assertFalse(HB().onecmd("create Review"))
+            id_attribute = test.getvalue().strip()
+        with patch("sys.stdout", new=SO()) as test:
+            cls = storage.all()["Review.{}".format(id_attribute)]
+            cmd = "Review.show({})".format(id_attribute)
+            self.assertFalse(HB().onecmd(cmd))
+            self.assertNotIn(cls, storage.all())
+        with patch("sys.stdout", new=SO()) as test:
+            self.assertFalse(HB().onecmd("create City"))
+            id_attribute = test.getvalue().strip()
+        with patch("sys.stdout", new=SO()) as test:
+            cls = storage.all()["City.{}".format(id_attribute)]
+            cmd = "City.show({})".format(id_attribute)
+            self.assertFalse(HB().onecmd(cmd))
+            self.assertNotIn(cls, storage.all())
+        with patch("sys.stdout", new=SO()) as test:
+            self.assertFalse(HB().onecmd("create State"))
+            id_attribute = test.getvalue().strip()
+        with patch("sys.stdout", new=SO()) as test:
+            cls = storage.all()["State.{}".format(id_attribute)]
+            cmd = "State.show({})".format(id_attribute)
+            self.assertFalse(HB().onecmd(cmd))
+            self.assertNotIn(cls, storage.all())
 
 
     def test_update(self):
@@ -141,17 +206,121 @@ class Test_HBNBCommand(unittest.TestCase):
             self.assertFalse(HB().onecmd("destroy USER"))
             output = "** class doesn't exist **"
             self.assertEqual(output, test.getvalue().strip())
-        
+        with patch("sys.stdout", new=SO()) as test:
+            self.assertFalse(HB().onecmd("USER.destroy()"))
+            output = "** class doesn't exist **"
+            self.assertEqual(output, test.getvalue().strip())
         with patch("sys.stdout", new=SO()) as test:
             self.assertFalse(HB().onecmd("destroy User"))
             output = "** instance id missing **"
             self.assertEqual(output, test.getvalue().strip())
+        with patch("sys.stdout", new=SO()) as test:
+            self.assertFalse(HB().onecmd("create User"))
+            id_attribute = test.getvalue().strip()
+        with patch("sys.stdout", new=SO()) as test:
+            cls = storage.all()["User.{}".format(id_attribute)]
+            cmd = "User.destroy({})".format(id_attribute)
+            self.assertFalse(HB().onecmd(cmd))
+            self.assertNotIn(cls, storage.all())
+        with patch("sys.stdout", new=SO()) as test:
+            self.assertFalse(HB().onecmd("create BaseModel"))
+            id_attribute = test.getvalue().strip()
+        with patch("sys.stdout", new=SO()) as test:
+            cls = storage.all()["BaseModel.{}".format(id_attribute)]
+            cmd = "BaseModel.destroy({})".format(id_attribute)
+            self.assertFalse(HB().onecmd(cmd))
+            self.assertNotIn(cls, storage.all())
+        with patch("sys.stdout", new=SO()) as test:
+            self.assertFalse(HB().onecmd("create Place"))
+            id_attribute = test.getvalue().strip()
+        with patch("sys.stdout", new=SO()) as test:
+            cls = storage.all()["Place.{}".format(id_attribute)]
+            cmd = "Place.destroy({})".format(id_attribute)
+            self.assertFalse(HB().onecmd(cmd))
+            self.assertNotIn(cls, storage.all())
+        with patch("sys.stdout", new=SO()) as test:
+            self.assertFalse(HB().onecmd("create Amenity"))
+            id_attribute = test.getvalue().strip()
+        with patch("sys.stdout", new=SO()) as test:
+            cls = storage.all()["Amenity.{}".format(id_attribute)]
+            cmd = "Amenity.destroy({})".format(id_attribute)
+            self.assertFalse(HB().onecmd(cmd))
+            self.assertNotIn(cls, storage.all())
+        with patch("sys.stdout", new=SO()) as test:
+            self.assertFalse(HB().onecmd("create Review"))
+            id_attribute = test.getvalue().strip()
+        with patch("sys.stdout", new=SO()) as test:
+            cls = storage.all()["Review.{}".format(id_attribute)]
+            cmd = "Review.destroy({})".format(id_attribute)
+            self.assertFalse(HB().onecmd(cmd))
+            self.assertNotIn(cls, storage.all())
+        with patch("sys.stdout", new=SO()) as test:
+            self.assertFalse(HB().onecmd("create City"))
+            id_attribute = test.getvalue().strip()
+        with patch("sys.stdout", new=SO()) as test:
+            cls = storage.all()["City.{}".format(id_attribute)]
+            cmd = "City.destroy({})".format(id_attribute)
+            self.assertFalse(HB().onecmd(cmd))
+            self.assertNotIn(cls, storage.all())
+        with patch("sys.stdout", new=SO()) as test:
+            self.assertFalse(HB().onecmd("create State"))
+            id_attribute = test.getvalue().strip()
+        with patch("sys.stdout", new=SO()) as test:
+            cls = storage.all()["State.{}".format(id_attribute)]
+            cmd = "State.destroy({})".format(id_attribute)
+            self.assertFalse(HB().onecmd(cmd))
+            self.assertNotIn(cls, storage.all())
 
     def test_all(self):
         with patch("sys.stdout", new=SO()) as test:
             self.assertFalse(HB().onecmd("all USER"))
             output = "** class doesn't exist **"
             self.assertEqual(output, test.getvalue().strip())
-        
+        with patch("sys.stdout", new=SO()) as test:
+            self.assertFalse(HB().onecmd("USER.all()"))
+            output = "** class doesn't exist **"
+            self.assertEqual(output, test.getvalue().strip())
+        with patch("sys.stdout", new=SO()) as test:
+            self.assertFalse(HB().onecmd("Amenity.all()"))
+            self.assertIn("Amenity", test.getvalue().strip())
+        with patch("sys.stdout", new=SO()) as test:
+            self.assertFalse(HB().onecmd("all Amenity"))
+            self.assertIn("Amenity", test.getvalue().strip())
+        with patch("sys.stdout", new=SO()) as test:
+            self.assertFalse(HB().onecmd("BaseModel.all()"))
+            self.assertIn("BaseModel", test.getvalue().strip())
+        with patch("sys.stdout", new=SO()) as test:
+            self.assertFalse(HB().onecmd("all BaseModel"))
+            self.assertIn("BaseModel", test.getvalue().strip())
+        with patch("sys.stdout", new=SO()) as test:
+            self.assertFalse(HB().onecmd("City.all()"))
+            self.assertIn("City", test.getvalue().strip())
+        with patch("sys.stdout", new=SO()) as test:
+            self.assertFalse(HB().onecmd("all City"))
+            self.assertIn("City", test.getvalue().strip())
+        with patch("sys.stdout", new=SO()) as test:
+            self.assertFalse(HB().onecmd("Place.all()"))
+            self.assertIn("Place", test.getvalue().strip())
+        with patch("sys.stdout", new=SO()) as test:
+            self.assertFalse(HB().onecmd("all Place"))
+            self.assertIn("Place", test.getvalue().strip())
+        with patch("sys.stdout", new=SO()) as test:
+            self.assertFalse(HB().onecmd("Review.all()"))
+            self.assertIn("Review", test.getvalue().strip())
+        with patch("sys.stdout", new=SO()) as test:
+            self.assertFalse(HB().onecmd("all Review"))
+            self.assertIn("Review", test.getvalue().strip())
+        with patch("sys.stdout", new=SO()) as test:
+            self.assertFalse(HB().onecmd("State.all()"))
+            self.assertIn("State", test.getvalue().strip())
+        with patch("sys.stdout", new=SO()) as test:
+            self.assertFalse(HB().onecmd("all State"))
+            self.assertIn("State", test.getvalue().strip())
+        with patch("sys.stdout", new=SO()) as test:
+            self.assertFalse(HB().onecmd("User.all()"))
+            self.assertIn("User", test.getvalue().strip())
+        with patch("sys.stdout", new=SO()) as test:
+            self.assertFalse(HB().onecmd("all User"))
+            self.assertIn("User", test.getvalue().strip())
 if __name__ == "__main__":
     unittest.main()
